e69b3512653debb4783c13291948abfda158a774
Sergey Gunin <sgunin@rambler.ru>
2016-11-03 18:10:12 +0300

    Add AssemblyInfo

--- EtollAssemblyInfo.cs
+++ EtollAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCompany("EToll")]
-[assembly: AssemblyCopyright("Copyright © 2017 EToll")]
+[assembly: AssemblyCopyright("Copyright © 2017 EToll ")]
 [assembly: AssemblyTrademark("http://etoll.net")]
